0321c4b6835e6107570b45e6a67fbd0f031a839e
gunacodee <gunaganesan2905@gmail.com>
2025-12-18 18:49:16 +0530

    Added database updates and API improvements

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_db',
+        'USER': 'postgres',
+        'PASSWORD': 'guna@2905',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
